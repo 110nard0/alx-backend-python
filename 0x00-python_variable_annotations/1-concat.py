3e111ff5bb84c4079f4fd6495e0bb75ae6aa4489
psych0wh1z <ikeagudike@gmail.com>
2023-08-03 17:09:57 +0100

    Add task1 files

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+"""Module 1-concat contains a type-annotated function that takes
+two strings as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Accepts two string parameters and returns a string
+
+    Args:
+        str1 (str): First string argument
+        str2 (str): Second string argument
+
+    Returns:
+        (str): concatenated string of str1 and str2
+    """
+    return "{}{}".format(str1, str2)
