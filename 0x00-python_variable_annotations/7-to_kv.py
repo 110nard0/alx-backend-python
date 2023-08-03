a86df2b4ea11cba845473564be18571a3aaeb860
psych0wh1z <ikeagudike@gmail.com>
2023-08-03 17:56:32 +0100

    Add task7 files

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,19 @@
+"""Module 7-to_kv contains a function that takes
+a string and an integer or float and returns a tuple
+"""
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Accepts a string and int/float as parameters
+    and returns a tuple consisting of both members
+
+    Args:
+        k (str): string positional argument
+        v (int | float): integer or float positional argument
+
+    Returns:
+        (tuple): contains string k and square of v
+    """
+    return (k, v ** 2)
