4af677bdd36ee3138650243c2cbb461cabfa347f
psych0wh1z <ikeagudike@gmail.com>
2023-08-03 17:17:47 +0100

    Add task2 files

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+"""Module 2-floor contains a type-annotated function that
+takes a float as argument and returns the floor
+"""
+
+
+def floor(n: float) -> int:
+    """Accepts a float parameter and returns its floor value
+
+    Args:
+        n (float): Float argument
+
+    Returns:
+        (int): floor of n
+    """
+    return int(n // 1)
