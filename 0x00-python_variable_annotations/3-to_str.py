7c6fc50617955cce4a2bbed52e465c470138bf7d
psych0wh1z <ikeagudike@gmail.com>
2023-08-03 17:22:08 +0100

    Add task3 files

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+"""Module 3-to_str contains a type-annotated function that
+takes a float as argument and returns its string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """Accepts a float parameter and returns its string representation
+
+    Args:
+        n (float): Float argument
+
+    Returns:
+        (str): string representation of n
+    """
+    return str(n)
