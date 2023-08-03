c1e4510beb134d32009f6ad803b2e88c9af6e0d9
psych0wh1z <ikeagudike@gmail.com>
2023-08-03 16:52:55 +0100

    Add task0 files

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+"""Module 0-add contains a type-annotated function that takes
+two floats as arguments and returns a float value
+"""
+
+def add(a: float, b: float) -> float:
+    """Accepts two float parameters and returns the sum
+
+    Args:
+        a (float): First float argument
+        b (float): Second float argument
+
+    Returns:
+        (float): sum of a and b
+    """
+    return a + b
