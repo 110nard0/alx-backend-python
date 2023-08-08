8277faca92245b03545704c9e98297b233a1ae9a
psych0wh1z <ikeagudike@gmail.com>
2023-08-08 22:24:18 +0100

    Add shebang and docstrings

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,16 @@
-"""
-"""
+#!/usr/bin/env python3
+"""Module 0-async_generator"""
 
 import asyncio
 import random
 
 
 async def async_generator() -> float:
-    """
+    """This coroutine takes no arguments, loops 10 times, each time
+    asynchronously waits 1 sec, then yields a rand number > 0 && < 10
+
+    Return:
+        Yields a random float value
     """
     for _ in range(10):
         await asyncio.sleep(1)
