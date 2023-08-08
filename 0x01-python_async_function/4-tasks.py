c78773743fc1e4c7f0988e2cf865c85fa9559afe
psych0wh1z <ikeagudike@gmail.com>
2023-08-08 01:14:17 +0100

    Add task4 files

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Module 4-tasks contains a function that spawns async processes
+and returns a list of their delays
+"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Async routine that spawns task_wait_random and returns
+    the list of all the delays (float values)
+    The list of the delays are in ascending order without using
+    sort() due to concurrency
+
+    Args:
+        n (int): number of task_wait_random functions spawned and called
+        max_delay (int): maximum seconds function can wait before returning
+
+    Returns:
+        delays (list of floats): list of wait time (in seconds) delayed
+    """
+    coroutines = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+
+    for task in asyncio.as_completed(coroutines):
+        delay = await task
+        delays.append(delay)
+
+    return delays
