bd14af2b1b740182d83b76c59f8146799891c667
psych0wh1z <ikeagudike@gmail.com>
2023-08-08 22:31:24 +0100

    Add generator type in function annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """This coroutine takes no arguments, loops 10 times, each time
     asynchronously waits 1 sec, then yields a rand number > 0 && < 10
 
