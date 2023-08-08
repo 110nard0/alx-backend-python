5957732ad01e26314721224982880bf3e0634097
psych0wh1z <ikeagudike@gmail.com>
2023-08-08 01:23:02 +0100

    Switch methods

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -22,7 +22,8 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
         delays (list of floats): list of wait time (in seconds) delayed
     """
-    coroutines = [task_wait_random(max_delay) for _ in range(n)]
+    coroutines = [asyncio.create_task(task_wait_random(max_delay))
+                  for _ in range(n)]
     delays = []
 
     for task in asyncio.as_completed(coroutines):
