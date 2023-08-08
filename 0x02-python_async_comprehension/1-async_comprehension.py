21e6563c6da52c790c90d9a67b191db331019641
psych0wh1z <ikeagudike@gmail.com>
2023-08-08 23:19:03 +0100

    Add task1 files

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+"""Module 1-async_comprehension
+"""
+
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Coroutine collects 10 random numbers using an async
+    comprehension over an async function then returns them
+
+    Return:
+        [float]: list of random float values
+    """
+    return [num async for num in async_generator()]
