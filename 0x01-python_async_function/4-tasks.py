343ae4486fae82e6dc3516fb5e1cf3c676f808c4
psych0wh1z <ikeagudike@gmail.com>
2023-08-08 09:14:26 +0100

    Revert to old code

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,19 +10,23 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Async routine that spawns the task_wait_random function and
+    """Async routine that spawns the task_wait_random task and
     returns the list of all the delays (float values)
     The list of the delays are in ascending order without using
     sort() due to concurrency
 
     Args:
-        n (int): number of task_wait_random functions spawned and called
+        n (int): number of task_wait_random tasks spawned and called
         max_delay (int): maximum seconds function can wait before returning
 
     Returns:
         delays (list of floats): list of wait time (in seconds) delayed
     """
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+    coroutines = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+
+    for task in asyncio.as_completed(coroutines):
+        delay = await task
+        delays.append(delay)
+
+    return delays
