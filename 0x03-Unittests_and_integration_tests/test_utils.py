ebb9eb638bebe2b9f8d8c4d3e5ad4e225a678bf0
psych0wh1z <ikeagudike@gmail.com>
2023-08-31 04:52:41 +0100

    Add function for testing exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,6 +8,7 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     """Inherits from unittest TestCase class"""
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -18,6 +19,17 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError('a')),
+        ({"a": 1}, ("a", "b"), KeyError('b'))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, exception):
+        """Tests access_nested_map function raises correct exception
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), str(exception))
 
 
 if __name__ == "__main__":
