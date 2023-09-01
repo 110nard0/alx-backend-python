3fabf0a415b901adfc0dff32f1e2d670e74da964
psych0wh1z <ikeagudike@gmail.com>
2023-09-02 00:34:59 +0100

    Implement integration tests for public_repos method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -125,6 +125,18 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         cls.get_patcher = patch("requests.get", side_effect=get_payload)
         cls.get_patcher.start()
 
+    def test_public_repos(self):
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos
+        )
+
+    def test_public_repos_with_license(self):
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos
+        )
+
     @classmethod
     def tearDownClass(cls) -> None:
         """Remove the class fixtures after running tests"""
