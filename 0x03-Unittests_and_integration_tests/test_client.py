f5041632d10f209a767b06a0c3d140b519b0813e
psych0wh1z <ikeagudike@gmail.com>
2023-09-01 23:28:05 +0100

    Test GithubOrgClient public instance method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -17,11 +17,25 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch("client.get_json")
     def test_org(self, org: str, response: Dict,
                  mocked_function: MagicMock) -> None:
+        """Test the org method returns expected result"""
         mocked_function.return_value = MagicMock(return_value=response)
         gh_org_client = GithubOrgClient(org)
         self.assertEqual(gh_org_client.org(), response)
         mocked_function.assert_called_once_with(
                 "https://api.github.com/orgs/{}".format(org))
+
+
+    def test_public_repos_url(self) -> None:
+        """Test the _public_repos_url instance method"""
+        with patch("client.GithubOrgClient.org",
+                   new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos"
+            )
 
 
 if __name__ == "__main__":
